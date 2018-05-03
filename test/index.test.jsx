3fad4abdf06e1cebeb1422be5bc9ff7b959f2e85
Shawn Seymour <shawn@devshawn.com>
2018-05-02 20:36:56 -0500

    Updating tests

--- test/index.test.jsx
+++ test/index.test.jsx
@@ -14,50 +14,50 @@ describe("react-dayjs main", () => {
 
     it("should render children", () => {
         const date = TestUtils.renderIntoDocument(<DayJS>{ DATE_STRING }</DayJS>)
-        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual("2000-01-31T11:59:00-06:00")
+        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual(dayjs(DATE_STRING).format())
     })
 
     it("should render date prop", () => {
         const date = TestUtils.renderIntoDocument(<DayJS date={ DATE_STRING }/>)
-        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual("2000-01-31T11:59:00-06:00")
+        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual(dayjs(DATE_STRING).format())
     })
 
     it("should render unix milliseconds", () => {
         const date = TestUtils.renderIntoDocument(<DayJS date={ DATE_STRING } unixMilliseconds/>)
-        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual("949341540000")
+        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual(`${dayjs(DATE_STRING).valueOf()}`)
     })
 
     it("should render unix seconds", () => {
         const date = TestUtils.renderIntoDocument(<DayJS date={ DATE_STRING } unixSeconds/>)
-        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual("949341540")
+        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual(`${dayjs(DATE_STRING).unix()}`)
     })
 })
 
 describe("react-dayjs manipulation", () => {
     it("should add", () => {
         const date = TestUtils.renderIntoDocument(<DayJS date={ DATE_STRING } add={ { hours: 1 } }/>)
-        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual("2000-01-31T12:59:00-06:00")
+        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual(dayjs(DATE_STRING).add(1, "hour").format())
     })
 
     it("should subtract", () => {
         const date = TestUtils.renderIntoDocument(<DayJS date={ DATE_STRING } subtract={ { hours: 1 } }/>)
-        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual("2000-01-31T10:59:00-06:00")
+        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual(dayjs(DATE_STRING).subtract(1, "hour").format())
     })
 })
 
 describe("react-dayjs toStrings", () => {
     it("should render toJSON", () => {
         const date = TestUtils.renderIntoDocument(<DayJS date={ DATE_STRING } toJSON/>)
-        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual("2000-01-31T17:59:00.000Z")
+        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual(dayjs(DATE_STRING).toJSON())
     })
 
     it("should render toISOString", () => {
         const date = TestUtils.renderIntoDocument(<DayJS date={ DATE_STRING } toISOString/>)
-        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual("2000-01-31T17:59:00.000Z")
+        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual(dayjs(DATE_STRING).toISOString())
     })
 
-    it("should render toObject", () => {
+    it("should render toString", () => {
         const date = TestUtils.renderIntoDocument(<DayJS date={ DATE_STRING } asString/>)
-        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual("Mon, 31 Jan 2000 17:59:00 GMT")
+        expect(ReactDOM.findDOMNode(date).innerHTML).toEqual(dayjs(DATE_STRING).toString())
     })
 })
