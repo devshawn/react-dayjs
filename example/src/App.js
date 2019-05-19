26ed17f5d6312e06f97a88deb0d6ce4266821555
Ganesh Poojary <ganesh4212@gmail.com>
2019-05-19 13:12:26 +0530

    fixes:#4 Added example react app.

---
+++ example/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DayJS from 'react-dayjs';
+import './App.css';
+
+function App() {
+  const date = "2000-01-31T12:59-0500";
+  return (
+    <div className="App">
+      <header className="App-header">      
+        <DayJS>{ date }</DayJS>
+      </header>
+    </div>
+  );
+}
+
+export default App;
