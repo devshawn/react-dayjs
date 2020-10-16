f144e5458fe652d47f212221478da35da517f2f4
Michael Lohmann <michael.lohmann@bevuta.com>
2020-10-16 07:56:32 +0000

    Transformed React.Component to Hooks-API

--- source/index.jsx
+++ source/index.jsx
@@ -1,77 +1,123 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { addToDate, generateInitialDate, subtractFromDate } from "./helpers"
+import React, {
+    useEffect,
+    useState,
+} from "react";
+import PropTypes from "prop-types";
+import {
+    addToDate,
+    generateInitialDate,
+    subtractFromDate,
+} from "./helpers";
 
-export default class DayJS extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { value: "" }
-    }
+export default DayJS = (props) => {
+    const [state, setState] = useState({
+        value: "",
+    });
 
-    componentWillMount() {
-        this.update(this.props)
-    }
+    useEffect(() => {
+        update(props);
+    }, []);
 
-    update() {
-        const { date, format, children, add, subtract, daysInMonth, toJSON, toISOString, asString, unixSeconds, unixMilliseconds, displayIsValid } = this.props
-        let dayjsDate = generateInitialDate(date, children)
+    const update = () => {
+        const {
+            date,
+            format,
+            children,
+            add,
+            subtract,
+            daysInMonth,
+            toJSON,
+            toISOString,
+            asString,
+            unixSeconds,
+            unixMilliseconds,
+            displayIsValid,
+        } = props;
+
+        let dayjsDate = generateInitialDate(date, children);
 
         if (add) {
-            dayjsDate = addToDate(dayjsDate, add)
+            dayjsDate = addToDate(dayjsDate, add);
         }
 
         if (subtract) {
-            dayjsDate = subtractFromDate(dayjsDate, subtract)
+            dayjsDate = subtractFromDate(dayjsDate, subtract);
         }
 
         if (displayIsValid) {
-            return this.setState({ value: `${dayjsDate.isValid()}` })
+            return setState((state) => ({
+                ...state,
+                value: `${dayjsDate.isValid()}`,
+            }));
         }
 
         if (daysInMonth) {
-            return this.setState({ value: dayjsDate.daysInMonth() })
+            return setState((state) => ({
+                ...state,
+                value: dayjsDate.daysInMonth(),
+            }));
         }
 
         if (toJSON) {
-            return this.setState({ value: dayjsDate.toJSON() })
+            return setState((state) => ({
+                ...state,
+                value: dayjsDate.toJSON(),
+            }));
         }
 
         if (toISOString) {
-            return this.setState({ value: dayjsDate.toISOString() })
+            return setState((state) => ({
+                ...state,
+                value: dayjsDate.toISOString(),
+            }));
         }
 
         if (asString) {
-            return this.setState({ value: dayjsDate.toString() })
+            return setState((state) => ({
+                ...state,
+                value: dayjsDate.toString(),
+            }));
         }
 
         if (unixMilliseconds) {
-            return this.setState({ value: dayjsDate.valueOf() })
+            return setState((state) => ({
+                ...state,
+                value: dayjsDate.valueOf(),
+            }));
         }
 
         if (unixSeconds) {
-            return this.setState({ value: dayjsDate.unix() })
+            return setState((state) => ({
+                ...state,
+                value: dayjsDate.unix()
+            }));
         }
 
         if (format) {
-            return this.setState({ value: dayjsDate.format(format) })
+            return setState((state) => ({
+                ...state,
+                value: dayjsDate.format(format),
+            }));
         }
 
-        return this.setState({ value: dayjsDate.format() })
-    }
+        return setState((state) => ({
+            ...state,
+            value: dayjsDate.format()
+        }));
+    };
 
-    render() {
-        const Element = this.props.element
-        return (
-            <Element>
-                { this.state.value }
-            </Element>
-        )
-    }
-}
+    const Element = props.element;
+    return <Element>{state.value}</Element>;
+};
 
 DayJS.propTypes = {
     element: PropTypes.any,
-    date: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array, PropTypes.object]),
+    date: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.array,
+        PropTypes.object,
+    ]),
     format: PropTypes.string,
     toJSON: PropTypes.bool,
     toISOString: PropTypes.bool,
@@ -82,8 +128,8 @@ DayJS.propTypes = {
     displayIsValid: PropTypes.bool,
     add: PropTypes.object,
     subtract: PropTypes.object,
-    children: PropTypes.string
-}
+    children: PropTypes.string,
+};
 
 DayJS.defaultProps = {
     element: "time",
@@ -98,5 +144,6 @@ DayJS.defaultProps = {
     displayIsValid: false,
     add: null,
     subtract: null,
-    children: null
-}
+    children: null,
+};
+
